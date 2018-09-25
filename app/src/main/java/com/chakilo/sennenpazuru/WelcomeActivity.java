3bce36040587bba44b414124e1f915451f124c5b
chenjialuo <chakilo@msn.com>
2018-09-25 17:20:15 +0800

    initial commit

---
+++ app/src/main/java/com/chakilo/sennenpazuru/WelcomeActivity.java
@@ -0,0 +1,13 @@
+package com.chakilo.sennenpazuru;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class WelcomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome);
+    }
+}
