5e7082389264c67287d879546c595029c675693d
chenjialuo <chakilo@msn.com>
2018-09-26 18:06:02 +0800

    calculator

---
+++ app/src/main/java/com/chakilo/sennenpazuru/view/CalculatorActivity.java
@@ -0,0 +1,35 @@
+package com.chakilo.sennenpazuru.view;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import com.chakilo.sennenpazuru.R;
+import com.chakilo.sennenpazuru.view.base.ActivityBase;
+
+public class CalculatorActivity extends ActivityBase {
+
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_calculator);
+//    }
+
+    @Override
+    protected int getLayoutResId() {
+        return R.layout.activity_calculator;
+    }
+
+    @Override
+    protected void doInitArgs(Bundle savedInstanceState) {
+
+    }
+
+    @Override
+    protected void doInitView() {
+
+    }
+
+    @Override
+    protected void doLoadData() {
+
+    }
+}
